0522039ae091b5a0bef5acdad75b5c440ff12a04
2300030083 <2300030083@kluniversity.in>
2026-03-15 22:54:15 +0530

    pushed

--- app/app.py
+++ app/app.py
@@ -1,5 +1,8 @@
 # app/app.py
+import sys
+import os
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
